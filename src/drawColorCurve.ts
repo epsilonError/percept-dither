c3b0e1f70d876fdc52326652bbf057d881a74114
epsilonError <99434682+epsilonError@users.noreply.github.com>
2025-05-30 18:36:33 +0000

    Outline in comments drawing prototype

--- src/drawColorCurve.ts
+++ src/drawColorCurve.ts
@@ -1,6 +1,12 @@
 import { gilbertPositions } from './gilbertCurve.ts';
 import { hilbertPositions } from './hilbertCurve.ts';
 
+// =====
+// SETUP
+// =====
+
+// ________________________________
+// Canvas Reference and image stats
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const context = canvas.getContext('2d')!;
@@ -8,11 +14,15 @@ const canvasWidth = 512;
 const canvasHeight = 342;
 const pixels = canvasWidth * canvasHeight;
 
+// _________________
+// Curve to Traverse
 /*const pos =*/ hilbertPositions(canvasWidth, canvasHeight);
 const pos = gilbertPositions(canvasWidth, canvasHeight);
 
-// Generate an array of 256 individual Bitmap grays
+// ___________________________________________________________
+// 256 Achromactic Pixels to draw on images (cached reference)
 const grays = new Array<ImageBitmap>(256);
+// Generate 256 individual Bitmap grays
 const grayBitmaps = {
   async *[Symbol.asyncIterator]() {
     for (let i = 0; i < 256; i++) {
@@ -23,16 +33,19 @@ const grayBitmaps = {
   },
 };
 
+/** Default colors used by Decker */
 const DECKER_DEFAULT_COLORS = [
   0xffffffff, 0xffffff00, 0xffff6500, 0xffdc0000, 0xffff0097, 0xff360097,
   0xff0000ca, 0xff0097ff, 0xff00a800, 0xff006500, 0xff653600, 0xff976536,
   0xffb9b9b9, 0xff868686, 0xff454545, 0xff000000,
 ] as const;
+/** 16 perceptually evenly spaced sRGB Grayscales */
 const GRAYSCALE_16_COLORS = [
   0xffffffff, 0xffececec, 0xffd9d9d9, 0xffc7c7c7, 0xffb4b4b4, 0xffa2a2a2,
   0xff919191, 0xff7f7f7f, 0xff6e6e6e, 0xff5e5e5e, 0xff4e4e4e, 0xff3e3e3e,
   0xff2e2e2e, 0xff1e1e1e, 0xff0d0d0d, 0xff000000,
 ] as const;
+/** Convert Hex values to equivalent RGB values (drops alpha channel) */
 function convertToRGB(n: number) {
   return [(n >> 16) & 0xff, (n >> 8) & 0xff, n & 0xff] as [
     number,
@@ -40,6 +53,7 @@ function convertToRGB(n: number) {
     number,
   ];
 }
+/** Iterable Default Decker Palette */
 const defaultPalette = {
   *[Symbol.iterator]() {
     for (const c of DECKER_DEFAULT_COLORS) {
@@ -47,6 +61,7 @@ const defaultPalette = {
     }
   },
 };
+/** Iterable 16 Grays Palette */
 const sixteenGraysPalette = {
   *[Symbol.iterator]() {
     for (const c of GRAYSCALE_16_COLORS) {
@@ -54,6 +69,9 @@ const sixteenGraysPalette = {
     }
   },
 };
+
+// _____________________________________________________________
+// 16 Color Pixels to draw on images (based on selected palette)
 const palette = new Array<ImageBitmap>(16);
 const paletteBitmaps = {
   async *[Symbol.asyncIterator](
@@ -67,12 +85,23 @@ const paletteBitmaps = {
   },
 };
 
+// ==========
+// Initialize
+// ==========
+
+/**
+ * Options for drawn colors
+ *
+ * - Flags for using only Black & White, 16 Grayscale Palette, or 16 Color Palette
+ * - Also a Step variable to manage animation speed
+ */
 interface Options {
   readonly blackAndWhite: boolean;
   readonly grayscale: boolean;
   readonly color: boolean;
   readonly steps: number;
 }
+/** Initializes Reference Palettes and Animation options*/
 async function main(
   blackAndWhite = true,
   grayscale = true,
@@ -103,8 +132,15 @@ async function main(
   return { blackAndWhite, grayscale, color: colors, steps } as const;
 }
 
+// =======
+// Animate
+// =======
+
+/** Place along the curve (prototype variable) */
 let place = 0;
+/** Reference to needed to cancel animation frame request */
 let animationID: number;
+/** Starting point of animation in AnimationTimeline */
 let zero: number;
 
 main()
@@ -114,6 +150,7 @@ main()
       return;
     }
     if (result.steps) {
+      // Schedules animation frames and number of pixels to draw per frame
       const multiDraw = (repeat: number) => {
         let count = 0;
         let p = pos.next();
@@ -125,7 +162,7 @@ main()
             p = pos.next();
           }
           if (p.value && !p.done) {
-            redrawCanvas(...p.value, result);
+            drawOnCanvas(...p.value, result);
           }
           count++;
         }
@@ -146,8 +183,9 @@ main()
       };
       animate(+(document.timeline.currentTime ?? 0));
     } else {
+      // Just draw over the canvas
       for (const p of pos) {
-        redrawCanvas(...p, result);
+        drawOnCanvas(...p, result);
       }
     }
   })
@@ -155,11 +193,25 @@ main()
     console.error(e);
   });
 
-function redrawCanvas(x: number, y: number, options: Options) {
+/**
+ * Draws a single pixel on the Canvas
+ * @param x x-position on the canvas
+ * @param y y-position on the canvas
+ * @param options Flags defining what palette should be used
+ * @returns
+ */
+function drawOnCanvas(x: number, y: number, options: Options) {
   if (!options.blackAndWhite && !options.grayscale && !options.color) {
     console.log('No palette to work from!');
     return;
   }
+  /**
+   * Index of the reference palette to use
+   *
+   * Currently selects based on place along the curve.
+   *
+   * @todo implement alternative selection criteria
+   */
   const select = options.color
     ? Math.round(((16 - 1) / pixels) * place)
     : options.grayscale
@@ -169,5 +221,5 @@ function redrawCanvas(x: number, y: number, options: Options) {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const bitmap = options.color ? palette[select]! : grays[select]!;
   context.drawImage(bitmap, x, y); // Uses ImageBitmap
-  place = place + 1;
+  place = place + 1; // used to prototype
 }
